84e8f72dead7552ddb301fa7c5ea7bdc5b01d6b4
Walleks Miranda <walleksmiranda@gmail.com>
2023-10-18 17:28:08 -0300

    feat: add filme mapper profile

---
+++ Profiles/FilmProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using filmesAPI.Data.Dtos;
+using filmesAPI.Models;
+
+namespace filmesAPI.Profiles;
+
+public class FilmProfile : Profile
+{
+  public FilmProfile()
+  {
+    CreateMap<CreateFilmDto, Filme>();
+    CreateMap<UpdateFilmDto, Filme>();
+  }
+}
